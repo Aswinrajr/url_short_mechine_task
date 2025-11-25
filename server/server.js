9fa7fac38b164a2c4c9c950ce5caca4166e82893
Aswinrajr <aswinrajr07@gmail.com>
2025-11-26 01:41:27 +0530

    Cors removed

--- server/server.js
+++ server/server.js
@@ -11,10 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: "https://url-short-mechine-task.vercel.app",
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 const connectDB = async () => {
